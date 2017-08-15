0eed957f58bdeb8af693a9890f6c23b84b452e98
Jeimy Mota <Jeimy Mota>
2017-08-14 21:37:27 -0400

    friend finder

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+    
+    {
+    "name":"Collete",
+    "photo":"https://i.pinimg.com/736x/c0/14/23/c014230dec32c2eeb133b7b8da072317--too-cute-cute-cute.jpg",
+    "scores": [
+    5,
+    1,
+    2,
+    2,
+    4,
+    5,
+    4,
+    7,
+    3,
+    5
+            ]
+    },
+    {
+    "name": "Morgan",
+    "photo": "http://www.cutestpaw.com/wp-content/uploads/2014/01/Augusta-has-reached-one-million-views.jpg",
+    "scores": [
+    1,
+    6,
+    5,
+    7,
+    3,
+    2,
+    1,
+    3,
+    4,
+    2
+            ]
+    },
+ {"name": "Widowmaker",
+    "photo": "https://blzgdapipro-a.akamaihd.net/media/thumbnail/widowmaker-gameplay.jpg",
+    "scores": [
+    1,
+    3,
+    5,
+    1,
+    5,
+    1,
+    3,
+    5,
+    3,
+    5
+            ]
+    },
+
+];
+
+module.exports = friends;
---
+++ app/routing/api-routes.js
@@ -0,0 +1,50 @@
+var friends = require("../data/friends.js");
+
+module.exports = function (app){
+    app.get("/api/friends", function(req, res){
+        res.json(friends);
+    });
+    
+    app.post("/api/friends", function(req, res){
+        var newBestie = {
+            name:"",
+            photo:"",
+            Diff:Infinity
+        };
+
+//        code below will take the results and parse it
+        var userData = req.body;
+        var Scores = userData.scores;
+        
+//        code below will calc the difference in user scores
+        var totalDiff;
+        
+//        looping through all the possibilities in database
+        for (var i = 0; i < friends.length; i++){
+            var currentbestie = friends[i];
+            totalDiff = 0;
+             
+            console.log(currentbestie.name);
+            
+//            looping through all friends
+            for (var k = 0; k < currentbestie.scores.length; k++){
+                var currentbestiescore = currentbestie.scores[k];
+                var currentUserScore = Scores[k];
+//                calculating deiff btwn scores and sums
+                totalDiff += Math.abs(parseInt(currentUserScore) - parseInt(currentbestiescore));
+            }
+//            if sum of diff is less than diff of currenbestie 
+            if (totalDiff <= newBestie.Diff){
+                newBestie.name = currentbestie.name;
+                newBestie.photo = currentbestie.photo;
+                newBestie.Diff = totalDiff;
+            }
+        }
+        
+        friends.push(userData);
+        
+        res.json(newBestie);
+        
+    });
+    
+};
